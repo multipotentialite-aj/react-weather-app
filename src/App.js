43490de6fb5d13f3e3357997dbcb138f56c6773c
AJ <79020277+multipotentialite-aj@users.noreply.github.com>
2021-08-08 21:30:54 -0400

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 
 export default function App() {
   return <div className="App">
+    <div className="container">
     <h1>Weather App</h1>
     <footer><a href="https://github.com/multipotentialite-aj/react-weather-app" target="_blank">Coded by AJ on GitHub</a></footer>
+  </div>
   </div>
 }
