4cbea99bf1c93dd8bcc6397c1439645d3d49b2e1
AJ <79020277+multipotentialite-aj@users.noreply.github.com>
2021-08-08 21:11:49 -0400

    Added GitHub repo url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,8 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>
+  return <div className="App">
+    <h1>Weather App</h1>
+    <footer><a href="https://github.com/multipotentialite-aj/react-weather-app" target="_blank">Coded by AJ on GitHub</a></footer>
+  </div>
 }
