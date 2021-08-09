d03b6598a2c136464fe88c6ffb51d4674dff8770
AJ <79020277+multipotentialite-aj@users.noreply.github.com>
2021-08-09 18:01:58 -0400

    Added functional date

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import FormattedDate from "./FormattedDate"
 import axios from "axios";
 import "./Weather.css";
 
@@ -14,7 +15,7 @@ export default function Weather(props) {
             description: response.data.weather[0].description,
             wind: response.data.wind.speed,
             city: response.data.name,
-            date: "Wednesday 07:00"
+            date: new Date(response.data.dt * 1000)
         });
     }
 
@@ -33,7 +34,9 @@ export default function Weather(props) {
                 </form>
                 <h1>{weatherData.city}</h1>
                 <ul>
-                    <li>{weatherData.date}</li>
+                    <li>
+                        <FormattedDate date={weatherData.date} />
+                    </li>
                     <li className="text-capitalize">{weatherData.description}</li>
                 </ul>
                 <div className="row">
