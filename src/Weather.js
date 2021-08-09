9ff7bf5c944561be73a3269e81c11db62abbccd3
AJ <79020277+multipotentialite-aj@users.noreply.github.com>
2021-08-08 22:45:23 -0400

    Updated some CSS

--- src/Weather.js
+++ src/Weather.js
@@ -7,10 +7,10 @@ export default function Weather() {
             <form>
                 <div className="row">
                     <div className="col-9">
-                <input type="search" placeholder="Enter a city" className="form-control"/>
+                <input type="search" placeholder="Enter a city" className="form-control" autoFocus="on"/>
                 </div>
                 <div className="col-3">
-                <input type="submit" value="Search" className="btn btn-primary"/>
+                <input type="submit" value="Search" className="btn btn-primary w-100"/>
                 </div>
                 </div>
             </form>
