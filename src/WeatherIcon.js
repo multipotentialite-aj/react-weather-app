b718cf3f99ee06a048ca03b997d31957b989a7a1
AJ <79020277+multipotentialite-aj@users.noreply.github.com>
2021-08-16 09:58:58 -0400

    Worked on icon colour

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="#1e1e1e"
+      color="#2692D0"
       size={props.size}
       animate={true}
     />
