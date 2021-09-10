7ba18e923b502c7b478581a43861292717f711d3
AJ <79020277+multipotentialite-aj@users.noreply.github.com>
2021-09-10 18:58:44 -0400

    Fixed forecast

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import FormattedDate from "./FormattedDate";
-import WeatherIcon from "./WeatherIcon";
+import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
-import WeatherTemperature from "./WeatherTemperature";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
     setWeatherData({
@@ -22,17 +22,33 @@ export default function Weather(props) {
     });
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
               <input
                 type="search"
-                placeholder="Enter a city"
+                placeholder="Enter a city.."
                 className="form-control"
                 autoFocus="on"
+                onChange={handleCityChange}
               />
             </div>
             <div className="col-3">
@@ -44,37 +60,11 @@ export default function Weather(props) {
             </div>
           </div>
         </form>
-        <h1 className="mb-2">{weatherData.city}</h1>
-        <ul>
-          <li>
-            <FormattedDate date={weatherData.date} />
-          </li>
-          <li className="text-capitalize">{weatherData.description}</li>
-        </ul>
-        <div className="row">
-          <div className="col-6">
-            <div className="clearfix">
-              <div className="float-left pt-2">
-                <WeatherIcon code={weatherData.icon} size={52} />
-                <WeatherTemperature celcius={weatherData.temperature} />
-              </div>
-            </div>
-          </div>
-
-          <div className="col-6">
-            <ul>
-              <li>Humidity: {weatherData.humidity}%</li>
-              <li>Wind: {weatherData.wind}km/h</li>
-            </ul>
-          </div>
-        </div>
+        <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
-    const apiKey = "ec8648d1df34ceeeb3c068934e0a6833";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
+    search();
     return "Loading...";
   }
 }
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,41 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celcius");
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function convertToCelcius(event) {
-    event.preventDefault();
-    setUnit("celcius");
-  }
-
-  if (unit === "celcius") {
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.celcius)}</span>
-        <span className="unit">
-          °C |
-          <a href="/" onClick={showFahrenheit}>
-            °F
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    let fahrenheit = (props.celsius * 9) / 5 + 32;
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(fahrenheit)}</span>
-        <span className="unit">
-          <a href="/" onClick={convertToCelcius}>
-            °C
-          </a>{" "}
-          | °F
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemperature">
+      <span className="temperature">{Math.round(props.celsius)}</span>
+      <span className="unit">°C</span>
+    </div>
+  );
 }
